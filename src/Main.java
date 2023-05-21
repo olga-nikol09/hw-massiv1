d090d29501909a4a80a47777161ce354c519980a
helga <helga-nikol09@yandex.ru>
2023-05-22 01:46:18 +0300

    first

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        task1 ();
+        task2 ();
+        task3 ();
+        task4 ();
+    }
+    public static void task1 (){
+        System.out.println("Задача1");
+    }
+    public static void task2 () {
+        System.out.println("Задача2");
+    }
+    public static void task3 () {
+        System.out.println("Задача3");
+    }
+    public static void task4 () {
+        System.out.println("Задача4");
+    }
+}
