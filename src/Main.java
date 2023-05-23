13568a57d2ce009fa5fc7f99e7d53f63117e41e7
helga <helga-nikol09@yandex.ru>
2023-05-23 13:35:08 +0300

    finish

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         task1 ();
@@ -7,14 +9,69 @@ public class Main {
     }
     public static void task1 (){
         System.out.println("Задача1");
+        int[] numbers = new int[3];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        double[] a = {1.57, 7.654, 9.986};
+        int[] apples = new int[5];
+
     }
     public static void task2 () {
         System.out.println("Задача2");
+        int[] numbers = new int[3];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        System.out.print(numbers[0] + ", ");
+        System.out.print(numbers[1] + ", ");
+        System.out.println(numbers[2]);
+        double[] a = {1.57, 7.654, 9.986};
+            System.out.print(a[0] + ", ");
+            System.out.print(a[1] + ", ");
+            System.out.println(a[2]);
+        int[] apples = new int[5];
+        apples[0] = 2;
+        apples[1] = 5;
+        apples[2] = 1;
+        for (int i = 0; i < 5; i++) {
+            System.out.print(apples[i] + ", ");
+        }
+        System.out.println();
     }
     public static void task3 () {
         System.out.println("Задача3");
+        int[] numbers = new int[3];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        System.out.print(numbers[2] + ", ");
+        System.out.print(numbers[1] + ", ");
+        System.out.println(numbers[0]);
+        double[] a = {1.57, 7.654, 9.986};
+        System.out.print(a[2] + ", ");
+        System.out.print(a[1] + ", ");
+        System.out.println(a[0]);
+        int[] apples = new int[5];
+        apples[0] = 2;
+        apples[1] = 5;
+        apples[2] = 1;
+        for (int i = 4; i >= 0; i--) {
+            System.out.print(apples[i] + ", ");
+        }
+        System.out.println();
     }
     public static void task4 () {
         System.out.println("Задача4");
+        int[] numbers = new int[3];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 == 1) {
+                numbers[i] += 1;
+            }
+        }
+        System.out.println(Arrays.toString(numbers));
     }
 }
